e5c9fb31cde06fc9597b9f782635f2883635d9be
bonohbonoh <psmb2006@naver.com>
2022-01-14 22:52:28 +0900

    Service Test

--- src/main/java/com/example/OEmbed/service/InstagramUrlService.java
+++ src/main/java/com/example/OEmbed/service/InstagramUrlService.java
@@ -1,12 +1,14 @@
 package com.example.OEmbed.service;
 
-import com.example.OEmbed.dto.ResponseDto;
+import com.example.OEmbed.dto.InstagramDto;
+import com.example.OEmbed.exception.PostNotFoundException;
 import com.example.OEmbed.exception.UrlMissMatchException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URISyntaxException;
@@ -22,18 +24,24 @@ public class InstagramUrlService implements IPlatformHtmlWriter {
     private final ObjectMapper objectMapper;
 
     @Override
-    public ResponseDto requestApi(String paramUrl) throws URISyntaxException, JsonProcessingException {
+    public InstagramDto requestApi(String paramUrl) throws URISyntaxException, JsonProcessingException {
 
         //인스타그램 포스트 확인
         boolean isInstagramPost = Pattern.compile("(https://www.instagram.com/p/.*?)").matcher(paramUrl).find();
         if (!isInstagramPost) {
-            throw new UrlMissMatchException("올바른 주소형식이 아닙니다.");
+            throw new UrlMissMatchException("올바른 Instagram 주소형식이 아닙니다.");
         }
 
         // API 요청
-        String response = restTemplate.getForObject(INSTAGRAM_URL + paramUrl, String.class);
 
-        return objectMapper.readValue(response, ResponseDto.class);
+        String response = "";
+        try {
+            response = restTemplate.getForObject(INSTAGRAM_URL + paramUrl, String.class);
+        } catch (HttpClientErrorException e) {
+            throw new PostNotFoundException("존재하지 않는 게시물이거나 O-embed에 등록되지 않은 게시물입니다.");
+        }
+
+        return objectMapper.readValue(response, InstagramDto.class);
     }
 
 }
