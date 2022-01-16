934b0ba9893ff64330ca8ed9350d5e52fac9c13d
bonohbonoh <psmb2006@naver.com>
2022-01-16 18:50:04 +0900

    ObjectMapper null ignore

--- src/main/java/com/example/OEmbed/config/ObjectMapperConfig.java
+++ src/main/java/com/example/OEmbed/config/ObjectMapperConfig.java
@@ -1,5 +1,7 @@
 package com.example.OEmbed.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         return objectMapper;
     }
 }
